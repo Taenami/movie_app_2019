82a3ab28a33b890d471154cf246bd2dacc26758e
dev-93 <cjb940103@gmail.com>
2019-12-07 18:38:29 +0900

    Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React from 'react';
 
-function Food({ fav }) {
-  return <h1>I like {fav}</h1>;
+function Food({ name, picture }) {
+  return (
+    <>
+      <h1>I like {name}</h1>;
+      <img src={picture} />;
+    </>
+  );
 }
+
+const foodList = [
+  {
+    name:"pizza",
+    image:"https://doughboyspizza.com/wp-content/uploads/2017/05/Foodz22.jpg"
+  },
+  {
+    name:"beef",
+    image:"https://halalexoticmeats.co.uk/wp-content/uploads/2015/01/A06248B7-5BEC-41C2-86A8-47618D02A231.jpeg"
+  },
+  {
+    name:"kimchi",
+    image:"http://www.maangchi.com/wp-content/uploads/2013/07/kimchi_1.jpg"
+  },
+  {
+    name:"samgeopsal",
+    image:"http://sites.psu.edu/jiyoonpassionblog/wp-content/uploads/sites/21669/2015/02/20140603181421_7159.jpg"
+  },
+  {
+    name:"chiecken",
+    image:"https://www.easyanddelish.com/wp-content/uploads/2016/08/One-Pot-Chicken-in-Lime-Coconut-Sauce-4-FromBrazilToYou.Org_.jpg"
+  }
+];
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="Odeng" />
-      <Food fav="Samgeopsal" />
+      {foodList.map(dish => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </>
   );
 }
