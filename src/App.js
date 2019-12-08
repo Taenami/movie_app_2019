4ae316df05142c3f85a1f0f2b369d503183df528
dev-93 <cjb940103@gmail.com>
2019-12-08 18:03:19 +0900

    learn component Life cycle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import PropTypes from "prop-types";
 
 class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    console.log("hello");
+  }
   state = {
     count: 0
   };
+
   add = () => {
     this.setState(current => ({
       count:current.count + 1
@@ -12,14 +17,26 @@ class App extends React.Component {
   };
     // state를 set할때 외부의 상태에 의존하지 않는 가장 좋은 방법 current function
 
-
   minus = () => {
     this.setState(current => ({
       count:current.count -1
     }));
   };
 
+  componentDidMount(){
+    console.log("component rendered");
+  }
+
+  componentDidUpdate(){
+    console.log("I just updated");
+  }
+
+  componentWillUnmount(){
+    console.log("Goodbye, component");
+  }
+
   render() {
+    console.log("I'm rendering");
     return (
       <>
         <h1>The number is: {this.state.count}</h1>
