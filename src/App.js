28c0d0f8fdb135af4dee77959ab36ca0c16111d1
dev-93 <cjb940103@gmail.com>
2019-12-08 18:18:21 +0900

    planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,20 @@
 import React from 'react';
 
 class App extends React.Component {
-
-  constructor(props){
-    super(props);
-    console.log("hello");
-  }
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   };
 
-  add = () => {
-    this.setState(current => ({
-      count:current.count + 1
-    }));
-  };
-    // state를 set할때 외부의 상태에 의존하지 않는 가장 좋은 방법 current function
-
-  minus = () => {
-    this.setState(current => ({
-      count:current.count -1
-    }));
-  };
-
-  componentDidMount(){
-    console.log("component rendered");
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false});
+    }, 2000);
   }
-
-  componentDidUpdate(){
-    console.log("I just updated");
-  }
-
-  componentWillUnmount(){
-    console.log("Goodbye, component");
-  }
-
+  
   render() {
-    console.log("I'm rendering");
-    return (
-      <>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
