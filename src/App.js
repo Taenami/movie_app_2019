2a5fecccf5532c6eaac474e19489bc72629db918
dev-93 <cjb940103@gmail.com>
2019-12-15 00:36:02 +0900

    Buliding the Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+
 
 function App(){
-  return <h1>Something</h1>;
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home}/>
+      <Route path="/about" component={About}/>
+    </HashRouter>
+  );
 }
 
 export default App;
