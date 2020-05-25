a506bc49bd2630ccdd1cc32c479204fe7c1a1bd9
dev-93 <cjb940103@gmail.com>
2020-05-26 02:09:32 +0900

    Updates

---
+++ precache-manifest.f498f6575e0159fcaaa59749506ab865.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa1e0644e61b61d50a66e2246b4273c",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "8887f34e4d5924606c63",
+    "url": "/movie_app_2020/static/css/main.4528e193.chunk.css"
+  },
+  {
+    "revision": "39b2b38184577ddecb15",
+    "url": "/movie_app_2020/static/js/2.c29b6b6e.chunk.js"
+  },
+  {
+    "revision": "8887f34e4d5924606c63",
+    "url": "/movie_app_2020/static/js/main.ae93a394.chunk.js"
+  },
+  {
+    "revision": "9767aebefe82a0b3a420",
+    "url": "/movie_app_2020/static/js/runtime~main.3339b485.js"
+  }
+]);
